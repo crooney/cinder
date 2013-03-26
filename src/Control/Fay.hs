8054db6f8bfe15d15b61df5a357a86bc480a5f1d
Chris Rooney <christopher.j.rooney@gmail.com>
2013-03-26 14:39:16 -0430

    http://www.haskell.org/haskellwiki/Pointfree
    unnecessary, really.

--- src/Control/Fay.hs
+++ src/Control/Fay.hs
@@ -16,17 +16,17 @@ ap :: Fay (a -> b) -> Fay a -> Fay b
 ap f x = f >>= \f' -> x >>= \x' -> return (f' x')
 
 mapM :: (a -> Fay b) -> [a] -> Fay [b]
-mapM f as = sequence (map f as)
+mapM = (sequence .) . map
 
 foldM :: (a -> b -> Fay a) -> a -> [b] -> Fay a
 foldM _ x [] = return x
 foldM f y (x:xs) = f y x >>= \z -> foldM f z xs
 
 zipWithM :: (a -> b -> Fay c) -> [a] -> [b] -> Fay [c]
-zipWithM f xs ys = sequence (zipWith f xs ys)
+zipWithM = ((sequence .) .) . zipWith
 
 zipWithM_ :: (a -> b -> Fay c) -> [a] -> [b] -> Fay ()
-zipWithM_ f xs ys = sequence_ (zipWith f xs ys)
+zipWithM_  = ((sequence_ .) .) . zipWith
 
 replicateM :: Int -> Fay a -> Fay [a]
-replicateM n x = sequence (replicate n x)
+replicateM = (sequence .) . replicate
