f16a92b7789d50732c7329b79fbb14d1ac300fe0
Chris Rooney <christopher.j.rooney@gmail.com>
2013-04-06 06:02:38 -0430

    only use bounce and settle for fx and use stagger.

--- examples/Bouncer.hs
+++ examples/Bouncer.hs
@@ -5,28 +5,22 @@ import FFI
 import Cinder.SVG
 import Cinder.SVG.Attributes
 
-main :: Fay ()
-main = addEventListener "load" bouncer False
-
 bouncer :: Fay ()
 bouncer = do
-    root >>= insert (b !+ s !+ rs !+ os !+ su !+ sd)
+    root >>= insert circ
+    root >>= byTag "circle" >>= stagger "cx" 50 100
+    root >>= byTag "animate" >>= zipInsert fx >>= stagger "begin" 0 2
     return ()
     where
-        d = 2.0
-        r = 4
-        b = markup !+ cRXY 25 50 100 ! fill "cyan" !+ aADR "cy" d r
-                !+ bounce 100 400 0.5 3 ! fill "freeze" !< Complete
-        s = markup !+ rHWXY 50 50 125 75 ! fill "red" !+ aADR "y" d r
-                !+ settle 75 375 0.35 2 ! fill "freeze" !< Complete
-        rs = markup !+ rHWXY 50 50 225 75 ! stroke "red" !+ aADR "y" d r
-                !+ runningStart 75 375 0.2 ! fill "freeze" !< Complete
-        os = markup !+ cRXY 25 350 100 ! stroke "cyan" !+ aADR "cy" d r
-                !+ overShoot 100 400 0.2 ! fill "freeze" !< Complete
-        su = markup !+ cRXY 25 450 50 ! fill "red" !+ aADR "cy" d r
-                !+ speedUp !+ ftN 100 400 ! fill "freeze" !< Complete
-        sd = markup !+ rHWXY 50 50 525 75 ! fill "cyan" !+ aADR "y" d r
-                !+ slowDown !+ ftN 75 375 ! fill "freeze" !< Complete
+        circ = foldl1 (!+) $ map go colors
+        go x = cRXY 25 50 100 ! fill x !+ aADR "cy" 2.0 1 ! fill "freeze"
+                    ! begin "indefinite" !< Complete
+        fx = [bounce 100 400 0.4 3, settle 100 400 0.35 3,
+              bounce 100 400 0.75 4, settle 100 400 0.65 4]
+        colors = ["lightpink", "lightblue", "lightsalmon", "lightgreen"]
+
+main :: Fay ()
+main = addEventListener "load" bouncer False
 
 addEventListener :: String -> Fay () -> Bool -> Fay ()
 addEventListener = ffi "window['addEventListener'](%1,%2,%3)"
