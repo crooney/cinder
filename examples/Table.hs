89ce75b4d148e04274e43eec4c02e4c7245bf674
Chris Rooney <christopher.j.rooney@gmail.com>
2013-04-20 03:17:19 -0430

    simple example for html stuff.

---
+++ examples/Table.hs
@@ -0,0 +1,27 @@
+module Table (main) where
+
+import Prelude hiding (id)
+import FFI
+import Control.Fay
+import Cinder.HTML
+import Cinder.HTML.Elements hiding (map,style)
+import Cinder.HTML.Attributes
+
+colors = go [ (r,g,b) | r <- s, g <- s, b <- s]
+    where s = [0, 0x33 .. 0xff]
+          go [] = []
+          go xs = map color (take 24 xs) : go (drop 24 xs)
+
+tableMU = markup ! table !+ foldl1 (!+) (map row colors) ! Complete
+    where row x = markup ! tr !+ foldl1 (!+) (map box x) ! Complete
+          box x = markup ! td ! sty x ! Complete
+          sty x = style ("height:20px;width:20px;background-color:" ++ x)
+
+htex :: Fay ()
+htex = root >>= byTag "body" >>= zipWithM_ insert [tableMU]
+
+main :: Fay ()
+main = addEventListener "load" htex False
+
+addEventListener :: String -> Fay () -> Bool -> Fay ()
+addEventListener = ffi "window['addEventListener'](%1,%2,%3)"
